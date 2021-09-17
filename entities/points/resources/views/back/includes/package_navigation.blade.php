49a26977bde5e99de71edcc1a17695b074cf42cb
Konstantin Bukin <bukin@inetstudio.ru>
2021-09-17 23:50:43 +0300

    Update menu

--- entities/points/resources/views/back/includes/package_navigation.blade.php
+++ entities/points/resources/views/back/includes/package_navigation.blade.php
@@ -1,3 +1,3 @@
-<li class="{{ isActiveRoute('back.addresses.points.*') }}">
+<li class="{{ isActiveRoute('back.addresses.points.*', 'mm-active') }}">
     <a href="{{ route('back.addresses.points.index') }}"><span class="nav-label">Точки</span></a>
 </li>
--- package/resources/views/back/includes/navigation.blade.php
+++ package/resources/views/back/includes/navigation.blade.php
@@ -1,6 +1,6 @@
-<li class="{{ isActiveRoute('back.addresses.*') }}">
-    <a href="#"><i class="fa fa-map-marker-alt"></i> <span class="nav-label">Адреса</span><span class="fa arrow"></span></a>
-    <ul class="nav nav-second-level collapse">
+<li class="{{ isActiveRoute('back.addresses.*', 'mm-active') }}">
+    <a href="#" aria-expanded="false"><i class="fa fa-map-marker-alt"></i> <span class="nav-label">Адреса</span><span class="fa arrow"></span></a>
+    <ul class="nav nav-second-level">
         @include('admin.module.addresses.points::back.includes.package_navigation')
     </ul>
 </li>
